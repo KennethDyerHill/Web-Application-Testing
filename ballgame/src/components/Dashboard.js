31690c23eb47572ccf194be7648533a4c3ae29e3
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-10-03 19:08:03 -0400

    Update

--- ballgame/src/components/Dashboard.js
+++ ballgame/src/components/Dashboard.js
@@ -9,9 +9,29 @@ export const addStrike = currentStrikes => {
 };
 
 export const addBall = currentBalls => {
-    if (currentBalls <= 3) {
+    if (currentBalls >= 3) {
         return 0;
     } else {
         return currentBalls + 1;
     }
 };
+
+export const addFoul = currentStrikes => {
+    if (currentStrikes >= 2) {
+        return currentStrikes;
+    } else {
+        return currentStrikes + 1;
+    }
+};
+
+export const addHit = currentValue => {
+    return (currentValue = 0);
+};
+
+export default function Dashboard() {
+    const [strike, setStrike] = useState(0);
+    const [ball, setBall] = useState(0);
+    return (
+        <div>
+    )
+}
